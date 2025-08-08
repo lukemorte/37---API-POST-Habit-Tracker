063350fd39b1e05b234596a9e30c6132af42ec49
Jakub Lukáš <jakublukas.eclairdesign@gmail.com>
2025-08-08 06:42:36 +0200

    Refactor API integration and add requirements file for dependencies

--- main.py
+++ main.py
@@ -7,9 +7,17 @@ import requests
 
 load_dotenv()
 
-PIXELA_ENDPOINT = os.getenv("API_PIXELA")
+PIXELA_API = os.getenv("PIXELA_API")
+PIXELA_TOKEN = os.getenv("PIXELA_TOKEN")
 USER_PARAMS = {
-    "token": 
+    "token": PIXELA_TOKEN,
+    "username": "lukemorte",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
 }
-
-requests.post()
+string = PIXELA_API + "/lukemorte"
+print(string)
+# response = requests.post(url=PIXELA_API, json=USER_PARAMS)
+response = requests.delete(string)
+response.raise_for_status()
+print(response)
